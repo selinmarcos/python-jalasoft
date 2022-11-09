2a3e94f01b56e27656f79f09e7a7f859fbd5e08f
Marcos Selin <marcosselinm@gmail.com>
2022-11-09 11:16:10 -0400

    get and save data into a txt file

--- e1.py
+++ e1.py
@@ -4,11 +4,36 @@
               # at least one lower letter
 import random
 import string
+import json
 
-#usamos la libreria random para generar numeros aleatorios le estamos -2 para añadir las 2 letras
+#usamos la libreria random para generar numeros aleatorios  -2 para añadir las 2 letras
 
-
+global count 
 pass_name = []
+mi_path = "db.txt"
+
+#reading file
+count = 0
+try:
+    text_file = open("db.txt", "r")
+    if count == 0 :
+        print('ENTRAMOS')
+        lines = text_file.readlines()
+        new_string = str(lines)[3:][:-3]
+        # print(new_string)
+        # a = json.loads(lines)
+        #aqui es la claveee tenemos que insertar con un iterador 'como dividir los elementos ?'
+        
+        # print(json.loads(lines))
+       
+        print(new_string.split("}."))
+        # pass_name.append(new_string)
+        # print(new_string)
+except:  
+    print("NO HAY DATOS")      
+
+
+#DOBLE BRACKETS ELIMINAR
 def generate_passoword():
     chars = random.randint(9, 16) - 2
 # print(chars)
@@ -32,6 +57,13 @@ def name_pass_saver(n):
     new_pass = generate_passoword()
     pass_name_dic = {"app_name":n, "app_pass":new_pass}
     pass_name.append(pass_name_dic)
+
+    #write into a txt
+    f = open("db.txt", "w")
+    f.write(str(pass_name))
+    
+    f.close()
+
     print('SUCCESFUL !')
 
 def get_app_pass(n):
@@ -43,15 +75,17 @@ while True:
     if user_input == '1':
         new_password = input("Type an App Name:")
         name_pass_saver(new_password)
+        count = 1
 
 
     elif user_input == '2':
         # print(password)
         app_to_get = input("Type the App Name that you want to get te password:")
+        # print(get_app_pass(app_to_get).app_name)
         print('Your App Name & Password are: \n'+ str(get_app_pass(app_to_get)))
         # NOTA: Como obtener el Key y Value de un Diccionario ? (No funciona hacerlo como en JS :-( ) 
 
         # print('Your App Name & Password are: '+ app_pwd.app_name + '\n' + app_pwd.app_pass)
-
+        
     elif user_input == '3':
         break    
